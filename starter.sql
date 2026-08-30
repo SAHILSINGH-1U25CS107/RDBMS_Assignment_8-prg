bbeef5da5c3cdbb216773572f4c69f68a9afade4
prabhak-rvs <prabhak@rvsgroup.com>
2026-08-30 10:07:29 +0530

    Create starter.sql

---
+++ starter.sql
@@ -0,0 +1,39 @@
+-- Employee Aggregate Functions Assignment
+
+-- Step 1: Create Employee table
+
+CREATE TABLE Employee (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(50),
+    Department VARCHAR(30),
+    Salary INT
+);
+
+-- Step 2: Insert the following records
+
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES
+(101, 'Ravi', 'HR', 25000),
+(102, 'Meena', 'IT', 40000),
+(103, 'Kumar', 'Finance', 35000),
+(104, 'Suresh', 'IT', 45000),
+(105, 'Latha', 'HR', 30000);
+
+
+-- Step 3: Write queries using COUNT(), MAX(), MIN()
+-- and AVG() on the Salary field.
+
+-- COUNT()
+-- Write your query below:
+
+
+-- MAX()
+-- Write your query below:
+
+
+-- MIN()
+-- Write your query below:
+
+
+-- AVG()
+-- Write your query below:
